6250bd37ebb5135e870d40b34cdfbc7be902270e
SISNCOM <100363703+SISNCOM@users.noreply.github.com>
2022-05-05 15:01:20 +0200

    Update Img class 05052022

--- Image/Img.py
+++ Image/Img.py
@@ -1,49 +1,67 @@
 import shutil
+from pathlib import PurePath
 from time import sleep
 from tqdm import tqdm
+from OBJSystem.Database.Database import Database
+import json
+import os
 
-
-# It's a class that represents an image
 class Img:
 
     def __init__(self, Cfg, Db):
-        """
-        The function __init__() is a constructor that initializes the class
-
-        :param Cfg: A ConfigParser object that contains the configuration information for the program
-        :param Db: The database object
-        """
         self.cfg = Cfg
         self.db = Db
+        self.fullPathFile=""
         self.path = ""
+        self.data = {}
         self.verbose = 0
 
-    def get_by_id(self, id):
-        """
-        It takes an id, and returns the path to the image
 
-        :param id: the id of the image
-        :return: The path to the image.
-        """
-        cr = self.db.get_cursor()
-        query = "select CONCAT(a.texte,'\\\\', i.text) " \
-                "from img i left join annotation " \
-                "a on a.type=14 and a.idelt=i.id_img " \
-                "and a.v1=i.nu_path where i.id_img={};".format(id)
-        cr.execute(query)
-        self.path, = cr.fetchone()
-        if self.verbose:
-            print("the path is : ", self.path,)
-        return self.path,
+    def info(self,id):
+        cursor = self.db.get_cursor()
+        query = "SELECT  id_img, texte as path, szx, szy, text as filename, nbimgcmpl as filesize  " \
+                " FROM img INNER JOIN annotation " \
+                "WHERE img.id_exp = annotation.idelt and img.nu_path = annotation.v1 and id_img = {};".format(id)
+        cursor.execute(query)
+        data = cursor.fetchall()
+        for row in data:
+            self.data = dict(id_img=row[0], path=row[1], szy=row[2], szx=row[3], filename=row[4])
+        if self.verbose:print(self.data['filename'])
+        return self.data
 
-    def copy(self):
-        """
-        It copies the file to a temporary location
-        """
+
+    def get_ext(self):
+        info = self.data["filename"]
+        ext = info.split(".")[-1]
+        if self.verbose: print(ext)
+        return ext
+
+    def getFullPath(self, mode):
+        if mode == 0:
+            hot_storage = self.cfg.get_data("PATH_VARIABLE", "path_hot","")
+            exp = self.data['path'].split("\\")[-1]+"\\"
+            self.fullPathFile = hot_storage + exp + str(self.data["id_img"]) +"_" + self.data["filename"]
+            try:
+                if os.path.exists(self.fullPathFile):
+                    return self.fullPathFile
+            except IOError as e:
+                if self.verbose: print(e)
+        elif mode == 1:
+            return self.fullPathFile
+        elif mode == 2:
+            cr = self.db.get_cursor()
+            query = "SELECT texte FROM annotation WHERE type=14 and id={}".format(id)
+            cr.execute(query)
+            res, = cr.fetchone()
+            return res,
+
+    def getK(self, extension):
+
+
+    def copy_imgTo(self):
         output = self.cfg.get_data("PATH_VARIABLE", "temp_copy", "")
-        for i in tqdm(range(5)):
+        for i in tqdm(range(10)):
             sleep(0.2)
         shutil.copy(self.path, output)
-        print("copy done")
 
 
